424e019b5734ac53796ca11d121b0bd46457ca88
Tuanthanh11 <thanhntph45933@fpt.edu.vn>
2024-06-10 21:33:48 +0700

    thanh

--- src/Views/Admin/category/index.blade.php
+++ src/Views/Admin/category/index.blade.php
@@ -48,7 +48,7 @@ Danh muc
                             <?= $cate['id'] ?>
                         </td>
                         <td>
-                            <img src="{{ asset($cate['image']) }}" alt="" width="100px">
+                            <img src="{{ asset($cate['image']) }}" alt="" width="100" height="100" style=" object-fit:cover">
                         </td>
 
                         <td>
